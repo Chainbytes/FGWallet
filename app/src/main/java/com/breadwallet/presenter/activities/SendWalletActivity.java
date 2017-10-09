bfb6373eb740cdca0f4b5d23dfc3bc2791a57dfd
abhishekmsipl <32363102+abhishekmsipl@users.noreply.github.com>
2017-10-09 21:50:39 +0530

    redirection and qr scan

--- app/src/main/java/com/breadwallet/presenter/activities/SendWalletActivity.java
+++ app/src/main/java/com/breadwallet/presenter/activities/SendWalletActivity.java
@@ -1,5 +1,6 @@
 package com.breadwallet.presenter.activities;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
@@ -17,7 +18,7 @@ import com.breadwallet.tools.manager.BRClipboardManager;
 import com.breadwallet.tools.security.RequestHandler;
 import com.breadwallet.wallet.BRWalletManager;
 
-public class SendWalletActivity extends AppCompatActivity {
+public class SendWalletActivity extends Activity {
     private ImageButton scanQRButton;
     public EditText addressEditText;
     Button payAddressFromClipboardButton;
@@ -46,135 +47,7 @@ public class SendWalletActivity extends AppCompatActivity {
             }
         });
 
-        payAddressFromClipboardButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final AlertDialog[] alert = {null};
-                final AlertDialog.Builder builder = new AlertDialog.Builder(SendWalletActivity.this);
-                if (BRAnimator.checkTheMultipressingAvailability()) {
 
-                    final String bitcoinUrl = BRClipboardManager.readFromClipboard(SendWalletActivity.this);
-                    String ifAddress = null;
-                    RequestObject obj = RequestHandler.getRequestFromString(bitcoinUrl);
-                    if (obj == null) {
-                        //builder.setTitle(getResources().getString(R.string.alert));
-                        builder.setMessage(getResources().getString(R.string.mainfragment_clipboard_invalid_data));
-                        builder.setNeutralButton(getResources().getString(R.string.ok),
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.dismiss();
-                                    }
-                                });
-                        alert[0] = builder.create();
-                        alert[0].show();
-                        BRClipboardManager.copyToClipboard(SendWalletActivity.this, "");
-                        addressEditText.setText("");
-                        return;
-                    }
-                    if (!addressEditText.getText().toString().isEmpty()) {
-                        ifAddress = addressEditText.getText().toString();
-                    } else {
-                        ifAddress = obj.address;
-                    }
-                    if (ifAddress == null) {
-                        //builder.setTitle(getResources().getString(R.string.alert));
-                        builder.setMessage(getResources().getString(R.string.mainfragment_clipboard_invalid_data));
-                        builder.setNeutralButton(getResources().getString(R.string.ok),
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.dismiss();
-                                    }
-                                });
-                        alert[0] = builder.create();
-                        alert[0].show();
-                        BRClipboardManager.copyToClipboard(SendWalletActivity.this, "");
-                        addressEditText.setText("");
-                        return;
-                    }
-//                    final String finalAddress = tempAddress;
-                    BRWalletManager wm = BRWalletManager.getInstance(SendWalletActivity.this);
-
-                    if (wm.isValidBitcoinPrivateKey(ifAddress) || wm.isValidBitcoinBIP38Key(ifAddress)) {
-                        BRWalletManager.getInstance(SendWalletActivity.this).confirmSweep(SendWalletActivity.this, ifAddress);
-                        addressEditText.setText("");
-                        return;
-                    }
-
-                    if (checkIfAddressIsValid(ifAddress)) {
-                        final BRWalletManager m = BRWalletManager.getInstance(SendWalletActivity.this);
-                        final String finalIfAddress = ifAddress;
-                        new Thread(new Runnable() {
-                            @Override
-                            public void run() {
-                                final boolean contained = m.addressContainedInWallet(finalIfAddress);
-                                final boolean used = m.addressIsUsed(finalIfAddress);
-                                SendWalletActivity.this.runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        if (contained) {
-
-                                            //builder.setTitle(getResources().getString(R.string.alert));
-                                            builder.setMessage(getResources().getString(R.string.address_already_in_your_wallet));
-                                            builder.setNeutralButton(getResources().getString(R.string.ok),
-                                                    new DialogInterface.OnClickListener() {
-                                                        public void onClick(DialogInterface dialog, int which) {
-                                                            dialog.dismiss();
-                                                        }
-                                                    });
-                                            alert[0] = builder.create();
-                                            alert[0].show();
-                                            BRClipboardManager.copyToClipboard(SendWalletActivity.this, "");
-                                            addressEditText.setText("");
-
-
-                                        } else if (used) {
-                                            builder.setTitle(getResources().getString(R.string.warning));
-
-                                            builder.setMessage(getResources().getString(R.string.address_already_used));
-                                            builder.setPositiveButton(getResources().getString(R.string.ignore),
-                                                    new DialogInterface.OnClickListener() {
-                                                        public void onClick(DialogInterface dialog, int which) {
-                                                            dialog.dismiss();
-                                                            FragmentScanResult.address = finalIfAddress;
-                                                            BRAnimator.animateScanResultFragment();
-                                                        }
-                                                    });
-                                            builder.setNegativeButton(getResources().getString(R.string.cancel),
-                                                    new DialogInterface.OnClickListener() {
-                                                        public void onClick(DialogInterface dialog, int which) {
-                                                            dialog.dismiss();
-                                                        }
-                                                    });
-                                            alert[0] = builder.create();
-                                            alert[0].show();
-                                        } else {
-//                                FragmentScanResult.address = finalAddress;
-//                                BRAnimator.animateScanResultFragment();
-                                            RequestHandler.processRequest((SendWalletActivity) SendWalletActivity.this, bitcoinUrl);
-                                        }
-                                    }
-                                });
-                            }
-                        }).start();
-
-                    } else {
-                        //builder.setTitle(getResources().getString(R.string.alert));
-                        builder.setMessage(getResources().getString(R.string.mainfragment_clipboard_invalid_data));
-                        builder.setNeutralButton(getResources().getString(R.string.ok),
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.dismiss();
-                                    }
-                                });
-                        alert[0] = builder.create();
-                        alert[0].show();
-                        BRClipboardManager.copyToClipboard(SendWalletActivity.this, "");
-                        addressEditText.setText("");
-                    }
-                }
-            }
-
-        });
 
     }
 
